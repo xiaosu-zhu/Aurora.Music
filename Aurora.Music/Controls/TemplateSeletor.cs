bb236ff9e2a8a6dc8a24f986f635ac15150edf41
朱筱苏 <Xiaosu.Zhu@outlook.com>
2017-10-27 07:53:03 +0800

    don't use genericviewmodel

---
+++ Aurora.Music/Controls/TemplateSeletor.cs
@@ -0,0 +1,36 @@
+﻿using Aurora.Music.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Aurora.Music.Controls
+{
+    class HomePageTemplateSeletor : DataTemplateSelector
+    {
+        public DataTemplate AlbumTemplate { get; set; }
+        public DataTemplate SongTemplate { get; set; }
+        
+
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            if (item is AlbumViewModel a)
+            {
+                return AlbumTemplate;
+            }
+            if (item is SongViewModel s)
+            {
+                return SongTemplate;
+            }
+            return null;
+        }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            return SelectTemplate(item);
+        }
+    }
+}
