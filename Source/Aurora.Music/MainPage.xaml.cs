85743f81bebfa47aab82c19744d935c1799b3516
朱筱苏 <Xiaosu.Zhu@outlook.com>
2017-12-06 08:05:32 +0800

    ui

--- Source/Aurora.Music/MainPage.xaml.cs
+++ Source/Aurora.Music/MainPage.xaml.cs
@@ -62,7 +62,7 @@ namespace Aurora.Music
                 {
                     InAppNotify.Dismiss();
                 });
-            }, TimeSpan.FromMilliseconds(1500));
+            }, TimeSpan.FromMilliseconds(3000));
         }
 
         private int lyricViewID;
